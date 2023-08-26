2c6f2ac39109ebccc32c0edcf5acdad885b31e60
GreyDragon55 <Jolton055@gmail.com>
2023-08-26 09:54:45 -0500

    update

---
+++ QuickDBD-crowdfunding_db_schema.sql - Copy.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/FHgL7y
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" Varchar   NOT NULL,
+    "description" Varchar   NOT NULL,
+    "goal" Varchar   NOT NULL,
+    "pledged" Varchar   NOT NULL,
+    "outcome" Varchar   NOT NULL,
+    "bankers_count" INT   NOT NULL,
+    "country" Varchar   NOT NULL,
+    "currency" INT   NOT NULL,
+    "launched_date" Varchar   NOT NULL,
+    "end_date" Varchar   NOT NULL,
+    "category_id" Varchar   NOT NULL,
+    "subcategory_id" Varchar   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" Varchar   NOT NULL,
+    "category" Varchar   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" Varchar   NOT NULL,
+    "last_name" Varchar   NOT NULL,
+    "email" Varchar   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" Varchar   NOT NULL,
+    "subcategory" Varchar   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
